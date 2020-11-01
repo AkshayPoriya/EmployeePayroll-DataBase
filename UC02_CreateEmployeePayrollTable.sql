8275e13eb0b960e0cde84a738af8ea7fbe25cf95
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-02 01:52:03 +0530

    [Akshay] UC2 Add Table

---
+++ UC02_CreateEmployeePayrollTable.sql
@@ -0,0 +1,9 @@
+-- Create Table 
+-- identity(starting_value, increament)
+CREATE TABLE employee_payroll
+(
+name VARCHAR(150) NOT NULL,
+id INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+salary FLOAT NOT NULL,
+start DATE NOT NULL
+);
