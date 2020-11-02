7e1035a29b95ea3edf2ee0cd1cc4751740554b48
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-02 09:07:33 +0530

    [Akshay] UC7 Aggregate functions

---
+++ UC07_AggregateFunctions.sql
@@ -0,0 +1,10 @@
+-- salary stats group by gender
+SELECT gender,
+SUM(salary) AS 'salary_sum',
+AVG(salary) AS 'salary_avg',
+MIN(salary) AS 'salary_min',
+MAX(salary) AS 'salary_max',
+COUNT(salary) AS 'salary_count'
+FROM employee_payroll
+GROUP BY gender;
+
